0e7753736f6b23dc1fc8a3d63a244913035fc10b
7799331667 <22495a4203@qiscet.edu.in>
2024-01-05 19:16:38 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/**-----------typing animation------------- */
+var typed = new Typed(".typing",{
+    strings:["Student","Developer","Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
